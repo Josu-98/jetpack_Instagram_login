c384325dd6f705f80bdcfe8e1a652efa96ad516e
Josue Ibarra <josue.ibarra998@gmail.com>
2024-06-17 12:03:34 -0500

    Added login header

---
+++ app/src/main/java/com/josu98/jetpack_instagram_login/LoginScreen.kt
@@ -0,0 +1,45 @@
+package com.josu98.jetpack_instagram_login
+
+import android.app.Activity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Header(Modifier.align(Alignment.TopEnd))
+        Body(Modifier.align(Alignment.Center))
+    }
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    Column(modifier = modifier) {
+
+    }
+}
+
+@Composable
+fun Header(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "Close button",
+        modifier = modifier.clickable { activity.finish() })
+}
